51f5b2fecab5b05160975aa4dba1a1d5e99702f3
John Mook <jedwardmook@yahoo.com>
2022-02-02 15:45:31 -0600

    commit tree 848cec49477da90f05ee9f7bfd08b73d8a3d7679

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ']+/g, "");
+  }
+  static titleize(string) {
+    const nonCaps = ['the','a','an','but','of','and','for','at','by','from']
+    let capString = string.split(' ').map(s => {
+      if(!nonCaps.includes(s)){
+        return Formatter.capitalize(s)
+      } else {
+        return s
+      }
+    }).join(' ')
+
+    return Formatter.capitalize(capString)
+  }
 }
